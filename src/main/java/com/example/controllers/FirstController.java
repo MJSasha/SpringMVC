24905c9f85538057a2bd706023b1e8bf525db740
MJSasha <sasha.ushankov@gmail.com>
2022-03-04 20:26:38 +0300

    Data to view

--- src/main/java/com/example/controllers/FirstController.java
+++ src/main/java/com/example/controllers/FirstController.java
@@ -1,16 +1,21 @@
 package com.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
         return "first/hello";
     }
 
